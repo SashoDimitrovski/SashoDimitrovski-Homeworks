2f934606f43380fb10ca3d13c651f5a0c6023dc1
SashoDimitrovski <61951531+SashoDimitrovski@users.noreply.github.com>
2020-03-25 13:35:35 +0100

    Add files via upload

---
+++ Homework_Strings_Methods/Homework_Strings_Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Homework_Strings_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            while (true)
+            {
+                string greeting = "Hello from SEDC Codecademy v7.0";
+                int stringLength = greeting.Length;
+                Console.WriteLine($"Please enter a number between 1 and {stringLength}");
+                string userInput = Console.ReadLine();
+                int n = 0;
+                bool parsedNum = int.TryParse(userInput, out n);
+
+                if (parsedNum)
+                {
+                    if (!(n > 1 && n < greeting.Length))
+                    {
+                        Console.WriteLine($"You must enter a number between 1 and {stringLength} ");
+                    }
+                    else
+                    {
+                        string subString = greeting.Substring(0, n);
+
+                        Console.WriteLine(subString);
+                        Console.WriteLine(stringLength);
+                    }
+                    Console.WriteLine(@"Press ""y"" to try again, otherwise, press any other key.");
+                    string tryAgain = Console.ReadLine();
+                    if (tryAgain.ToLower() == "y")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Good bye!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a number");
+                }
+                continue;
+            }
+            Console.ReadLine();
+
+
+            #endregion
+
+        }
+
+    }
+}
---
+++ Homework_Strings_Methods/Task 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Please enter a sentence!");
+                string sentenceString = Console.ReadLine();
+                Console.WriteLine("Now, please enter any character!");
+                string character = Console.ReadLine();
+                char ch = '0';
+                bool chParsed = char.TryParse(character, out ch);
+                int k = sentenceString.Length;
+                char[] sentenceCharacters = sentenceString.ToCharArray();
+                int count = 0;
+                if (chParsed)
+                {
+                    foreach (char c in sentenceCharacters)
+                    {
+                        if (c == ch)
+                        {
+                            count++;
+                        }
+                    }
+                    Console.WriteLine($"The character that you entered appears: {count} times.");
+                    Console.WriteLine(@"Press ""y"" to try again, otherwise, press any other key.");
+                    string tryAgain = Console.ReadLine();
+                    if (tryAgain.ToLower() == "y")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Good bye!");
+                        break;
+                    }
+
+                }
+                Console.ReadLine();
+                
+                
+                
+
+
+            }
+            
+        }
+    }
+}
