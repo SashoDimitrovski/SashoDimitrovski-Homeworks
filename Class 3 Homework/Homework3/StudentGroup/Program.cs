da41ed8dffd49136d233c834541673e0ba22a8b0
SashoDimitrovski <61951531+SashoDimitrovski@users.noreply.github.com>
2020-03-25 13:32:22 +0100

    Add files via upload

---
+++ Class 3 Homework/Homework3/StudentGroup/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] studentsG1 = { "Ivan", "Bojan", "Goran", "Saso", "Petar" };
+                string[] studentsG2 = { "Vesna", "Marija", "Tanja", "Donka", "Ivana" };
+                Console.WriteLine("Please enter 1 or 2 to select a group!");
+                string input1or2 = Console.ReadLine();
+                int user1or2 = 0;
+                bool parseNumbers = int.TryParse(input1or2, out user1or2);
+                if (parseNumbers)
+                {
+
+                    if (user1or2 == 1)
+                    {
+                        Console.WriteLine("The students in this group are: ");
+                        foreach (var n in studentsG1)
+                        {
+
+                            Console.WriteLine(n);
+                        }
+
+                                        }
+                    else
+                    {
+                        if (user1or2 == 2)
+                        {
+                            Console.WriteLine("The students in this group are: ");
+                            foreach (var n in studentsG2)
+                            {
+                                Console.WriteLine(n);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter 1 or 2!");
+                        }
+
+                    }
+
+
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.WriteLine("I said, enter 1 or 2!");
+                }
+            }
+        }
+    }
+}
---
+++ Class 3 Homework/Homework3/SumOfEven/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+                int[] numbers = new int[6];
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine("Please enter a number");
+                    string inputNum = Console.ReadLine();
+                    int nums;
+                    bool parseNums = int.TryParse(inputNum, out nums);
+                    if (parseNums)
+                    {
+                        numbers[i] = nums;
+                        if (numbers[i] % 2 == 0)
+                        {
+                            sum += numbers[i];
+                        }
+                    }
+                    else
+                    {
+                        if (!parseNums)
+                        {
+                            Console.WriteLine("You didn't enter a real number. Please enter a real number!");
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("The sum of the even numbers in the array is: " + sum);
+                Console.ReadLine();
+
+
+
+            }
+            
+            
+            
+            
+
+            
+
+
+
+
+        }
+    }
+}
